2bfd384e44461f5da7162a25cf4c64238cce215c
Alejandro Zuluaga <azuluaga@MacBook-Air-de-Alejandro.local>
2026-01-30 07:40:05 -0500

    cambios en main

--- App/view.py
+++ App/view.py
@@ -55,9 +55,7 @@ def print_menu():
     print("Opciones:")
     print("1- Cargar Libros")
     print("2- Cargar Tags")
-    # TODO: Mods de Est-1, Est-2 y Est-3 en el Lab 2
-    # Agregue la opción 3 para cargar los tags de los libros.
-    # Pueede guiarse de las opciones 1 y 2.
+    # TODO: Mods de Est-2. Agregar la opción 3 en el menú
     print("3- Cargar Tags de Libros")
     print("0- Salir")
 
